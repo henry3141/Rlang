1e3e6e22df35897382a276f703f708dc8aae194e
henry3141 <100469145+henry3141@users.noreply.github.com>
2022-08-04 14:46:47 +0100

    Default files for a Rust envirement
    Setup envirement.

---
+++ target/debug/deps/rlang-995feab265ba3c38.d
@@ -0,0 +1,5 @@
+c:\Users\luka1\OneDrive\Documents\GitHub\rlang\target\debug\deps\rlang-995feab265ba3c38.rmeta: src\main.rs
+
+c:\Users\luka1\OneDrive\Documents\GitHub\rlang\target\debug\deps\rlang-995feab265ba3c38.d: src\main.rs
+
+src\main.rs:
